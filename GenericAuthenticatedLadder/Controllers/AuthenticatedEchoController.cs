e5e6abf144225471271de94c2ae9125e8b040247
Alessandro Lensi <nomadster@gmail.com>
2019-01-02 18:34:20 +0100

    First commit with euthenticated echo api to test

---
+++ GenericAuthenticatedLadder/Controllers/AuthenticatedEchoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericAuthenticatedLadder.Controllers
+{
+    public class AuthenticatedEchoController : ControllerBase
+    {
+        [Authorize, HttpGet("echo")]
+        public async Task<JsonResult> DoAuthenticatedEcho()
+        {
+            var jwt = await HttpContext.Authentication.GetAuthenticateInfoAsync("Bearer");
+            return new JsonResult(jwt.Properties.Items);
+        }
+    }
+}
