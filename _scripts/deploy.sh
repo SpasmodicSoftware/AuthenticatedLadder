901f0cd3dfa34ebb467b417018b78d9fc0a8ecba
Alessandro Lensi <nomadster@gmail.com>
2019-01-05 19:27:34 +0100

    Attenting automatic deploy to DO optiomization - take 1

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,14 +3,11 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _deploy, and push it to our server.
-    git init
         
     git remote add deploy "mnemosyne@165.227.132.89:/home/mnemosyne/authenticatedLadder"
     git config user.name "Travis CI"
     git config user.email "nomadster+authenticatedLadderTravisCI@gmail.com"
     
-    git add .
-    git commit -m date | xargs echo "Deploy $1"
     git push --force deploy master
 else
     echo "Not deploying, since this branch isn't master."
