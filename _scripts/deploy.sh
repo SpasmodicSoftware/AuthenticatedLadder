87640a1fab822bf4c4488849d7efc754dbf428aa
Alessandro Lensi <nomadster@gmail.com>
2019-01-05 18:48:54 +0100

    date

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "It works"
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    # Initialize a new git repo in _deploy, and push it to our server.
+    mkdir _deploy
+    cd _deploy
+    git init
+        
+    git remote add deploy "mnemosyne@"
+    git config user.name "Travis CI"
+    git config user.email "nomadster+authenticatedLadderTravisCI@gmail.com"
+    
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi
