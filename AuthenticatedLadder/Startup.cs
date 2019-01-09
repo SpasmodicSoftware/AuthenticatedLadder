c917f0b7e6097b1df97e4bb9873ac8c02ccc4a7f
Alessandro Lensi <nomadster@gmail.com>
2019-01-09 21:07:04 +0100

    We do not need CORS

--- AuthenticatedLadder/Startup.cs
+++ AuthenticatedLadder/Startup.cs
@@ -35,19 +35,7 @@ namespace GenericAuthenticatedLadder
             {
                 app.UseDeveloperExceptionPage();
             }
-            //else
-            //{
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
-            // global cors policy
-            app.UseCors(x => x
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials());
 
-            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
