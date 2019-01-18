5b6704f3fda0a1b8d57b24124bb720cd639a38c5
Alessandro Lensi <nomadster@gmail.com>
2019-01-18 01:14:59 +0100

    Trying to build docker image on travis CI, Take 2

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,6 +3,7 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
         docker build -t spasmodicsoftware/authenticated-ladder:latest .
+	docker images
     # Initialize a new git repo in _deploy, and push it to our server.
         
 #    git remote add deploy "mnemosyne@165.227.132.89:/home/mnemosyne/authenticatedLadder"
