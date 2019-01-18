ccadd53d85966d459af6674bd244c3696afaa069
Alessandro Lensi <nomadster@gmail.com>
2019-01-18 01:03:25 +0100

    Trying to build docker image on travis CI

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,13 +2,14 @@
 
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
+        docker build -t spasmodicsoftware/authenticated-ladder:latest .
     # Initialize a new git repo in _deploy, and push it to our server.
         
-    git remote add deploy "mnemosyne@165.227.132.89:/home/mnemosyne/authenticatedLadder"
-    git config user.name "Travis CI"
-    git config user.email "nomadster+authenticatedLadderTravisCI@gmail.com"
+#    git remote add deploy "mnemosyne@165.227.132.89:/home/mnemosyne/authenticatedLadder"
+#    git config user.name "Travis CI"
+#    git config user.email "nomadster+authenticatedLadderTravisCI@gmail.com"
     
-    git push --force deploy master
+#    git push --force deploy master
 else
     echo "Not deploying, since this branch isn't master."
 fi
