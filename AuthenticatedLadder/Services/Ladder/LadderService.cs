15b8b078e3d30a70670ac67a5db13145a1c94ca2
Alessandro Lensi <nomadster@gmail.com>
2019-01-26 17:59:04 +0100

    Partial implementation of business logic

--- AuthenticatedLadder/Services/Ladder/LadderService.cs
+++ AuthenticatedLadder/Services/Ladder/LadderService.cs
@@ -6,28 +6,75 @@ namespace AuthenticatedLadder.Services.Ladder
 {
     public class LadderService : ILadderService
     {
-
-        public LadderService()
+        private ILadderRepository _repository;
+        public LadderService(ILadderRepository repository)
         {
-            //TODO Injectare repository e logger
+            //TODO Injectare logger
+            _repository = repository;
+        }
+
+        private bool isValidGetEntryForUserInput(string ladderId, string platform, string username)
+        {
+            return !string.IsNullOrEmpty(ladderId)
+                && !string.IsNullOrEmpty(platform)
+                && !string.IsNullOrEmpty(username);
         }
 
         public LadderEntry GetEntryForUser(string ladderId, string platform, string username)
         {
-            //TODO Implement me
-            throw new NotImplementedException();
+            if(!isValidGetEntryForUserInput(ladderId,platform,username))
+            {
+                return null;
+            }
+            LadderEntry result;
+            try
+            {
+                result = _repository.GetEntryForUser(ladderId, platform, username);
+            } 
+            catch(Exception)
+            {
+                //TODO log
+                throw;
+            }
+            return result;
         }
 
         public List<LadderEntry> GetTopEntries(string ladderId)
         {
-            //TODO Implement me
-            throw new NotImplementedException();
+            var result = new List<LadderEntry>();
+
+            if (!string.IsNullOrEmpty(ladderId))
+            {
+
+                try
+                {
+                    result = _repository.GetTopEntries(ladderId);
+                }
+                catch (Exception)
+                {
+                    //TODO log error
+                    throw;
+                }
+            }
+            return result;
         }
 
         public LadderEntry Upsert(LadderEntry entry)
         {
-            //TODO Implement me
-            throw new NotImplementedException();
+            LadderEntry result = null;
+            if(entry != null)
+            {
+                try
+                {
+                    result = _repository.Upsert(entry);
+                }
+                catch(Exception)
+                {
+                    //TODO logme
+                    throw;
+                }
+            }
+            return result;
         }
     }
 }
