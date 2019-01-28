b20b0ef7143a9ee710536364d3f285090a137365
Alessandro Lensi <alessandro.lensi@apparound.com>
2019-01-28 17:59:19 +0100

    Reverted minicover.sh

---
+++ AuthenticatedLadder.UnitTests/Services/Ladder/LadderServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Moq;
+using AuthenticatedLadder.DomainModels;
+
+namespace AuthenticatedLadder.UnitTests.Services.Ladder
+{
+    public class LadderServiceTest
+    {
+        private ILadderRepository _repository;
+
+        public LadderServiceTest()
+        {
+            _repository = Mock.Of<ILadderRepository>();
+        }
+
+
+    }
+}
