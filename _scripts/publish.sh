a7c859b5b6d206a89b8580feda3d23b019459cb9
Alessandro Lensi <nomadster@gmail.com>
2019-01-28 01:25:15 +0100

    Added hash for commit

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -3,7 +3,7 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-
+    COMMIT=${TRAVIS_COMMIT::8}
     REPO=spasmodicsoftware/authenticated-ladder
     docker build -t $REPO:$COMMIT .
     docker tag $REPO:$COMMIT $REPO:latest    
