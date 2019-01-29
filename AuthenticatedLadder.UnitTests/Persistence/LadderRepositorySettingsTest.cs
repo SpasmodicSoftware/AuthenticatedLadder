f5ed32646e119080701143fd55bb6e4dd3d74527
Alessandro Lensi <nomadster@gmail.com>
2019-01-29 21:02:40 +0100

    Tested IsValidConfiguration() or LadderRepositorySettings because I care of my software quality :P

---
+++ AuthenticatedLadder.UnitTests/Persistence/LadderRepositorySettingsTest.cs
@@ -0,0 +1,24 @@
+﻿using AuthenticatedLadder.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AuthenticatedLadder.UnitTests.Persistence
+{
+    public class LadderRepositorySettingsTest
+    {
+        [Fact]
+        public void ASettingsIsValidWhenLengthIsMoreThanZero()
+        {
+            var settings = new LadderRepositorySettings();
+
+            settings.Length = 0;
+            Assert.False(settings.IsValidConfiguration());
+
+            settings.Length = 25;
+            Assert.True(settings.IsValidConfiguration());
+           
+        }
+    }
+}
