ad6fef51405479026c2c4962c7ea26647f8ee4f9
Alessandro Lensi <nomadster@gmail.com>
2019-02-01 01:29:51 +0100

    ControllerTest needs to imnplement IClassFixture

--- AuthenticatedLadder.IntegrationTests/Controllers/LadderControllerTest.cs
+++ AuthenticatedLadder.IntegrationTests/Controllers/LadderControllerTest.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using GenericAuthenticatedLadder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AuthenticatedLadder.IntegrationTests.Controllers
 {
-    public class LadderControllerTest
+    public class LadderControllerTest : IClassFixture<WebApplicationFactory<Startup>>
     {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public LadderControllerTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
     }
 }
