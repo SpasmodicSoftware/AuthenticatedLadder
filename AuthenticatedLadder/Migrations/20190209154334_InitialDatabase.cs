0da3da5380661fcc2cc9901d07e0f860796dd651
Alessandro Lensi <nomadster@gmail.com>
2019-02-09 17:47:06 +0100

    Added migrations

---
+++ AuthenticatedLadder/Migrations/20190209154334_InitialDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthenticatedLadder.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ladders",
+                columns: table => new
+                {
+                    LadderId = table.Column<string>(nullable: false),
+                    Platform = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(nullable: false),
+                    Score = table.Column<long>(nullable: false),
+                    EntryDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ladders", x => new { x.LadderId, x.Platform, x.Username });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ladders");
+        }
+    }
+}
